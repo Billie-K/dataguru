27ecf57f81f9815ab8464503da1dc52f4779c733
Billie-K <billiemush@gmail.com>
2022-06-14 01:12:08 +0200

    build

--- src/framework/rest/client/http-client.ts
+++ src/framework/rest/client/http-client.ts
@@ -7,7 +7,7 @@ import { useToken } from '@/lib/hooks/use-token';
 
 const Axios = axios.create({
   baseURL: process.env.NEXT_PUBLIC_REST_API_ENDPOINT,
-  timeout: 5000,
+  timeout: 20000,
   headers: {
     'Content-Type': 'application/json',
   },
